6c90c3d9c8c85bd887c5f57c2f24055d4a560cf9
6ink0 <angel.6ink0@gmail.com>
2022-11-20 20:14:56 -0300

    Se crea clase 'Producto', regiones y requerimientos según el caso de estudio.

---
+++ PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/Producto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel
+{
+    class Producto
+    {
+        /**
+         * “TRUEQUE” es una compañía que se dedica al intercambio de objetos (Atributo de Clase: Producto (Código, Nombre, 
+         * Descripción)), dispone de una bodega donde las personas (Atributos de Clase: Producto (ClienteId)) van a dejar 
+         * sus artículos y se les pregunta por cual objeto desean intercambiarlo (Atributo de Clase: Producto (ValorAprox, 
+         * Preferencias[])), buscan en una planilla Excel si existe para ofrecer el trueque, también buscan en planillas 
+         * históricas si en algún momento (Atributo de Clase: Producto (FechaIngreso, Disponible)) hubo alguien con el 
+         * producto así se puede contactar al dueño e informarle que existe posibilidad de intercambio si tiene otro 
+         * producto igual o similar. 
+         */
+
+        #region Propiedades
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Métodos
+        #endregion
+    }
+}
