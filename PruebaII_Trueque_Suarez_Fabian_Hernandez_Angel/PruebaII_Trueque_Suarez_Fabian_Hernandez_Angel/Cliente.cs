86bd56957397d6c84969bfc5b7a6450303ef2557
6ink0 <angel.6ink0@gmail.com>
2022-11-20 19:34:19 -0300

    Se crea clase 'Cliente' según los requerimientos del caso de estudio.

---
+++ PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel
+{
+    class Cliente
+    {
+        /**
+         * “TRUEQUE” es una compañía que se dedica al intercambio de objetos, dispone de una bodega 
+         * donde las personas --> Clase: Cliente <-- van a dejar sus artículos...
+         * ...si en algún momento hubo alguien con el producto así se puede 
+         * contactar al dueño (Atributos de Clase: Cliente (nombre, rut y fono))...
+         */
+
+
+
+
+    }
+}
