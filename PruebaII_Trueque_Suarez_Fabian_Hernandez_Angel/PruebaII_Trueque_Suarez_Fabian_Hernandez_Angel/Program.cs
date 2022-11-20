cff109e9b27c5d95d073a4dc6dc006cfa32b5aa8
6ink0 <angel.6ink0@gmail.com>
2022-11-20 18:55:55 -0300

    Test de guardado en Github

---
+++ PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel
+{
+    class Program
+    {
+        //test de guardado en Github.
+    }
+}
