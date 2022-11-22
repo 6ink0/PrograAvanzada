1de7560cf02a96a306d3112dfcd74352d4f53ae9
6ink0 <angel.6ink0@gmail.com>
2022-11-21 23:23:33 -0300

    Se crean estructuras del los métodos 'RellenarListaClientes' y 'RellenarListaProductos' de la clase 'GestionProducto'.

--- PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/GestionProducto.cs
+++ PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/GestionProducto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,51 @@ namespace PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel
         static List<Cliente> listaCliente;
         static List<Producto> listaProducto;
         static string rutaCliente = @"C:\Users\ginko\Documents\GitHub\PruebaII_Trueque\PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel\PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel\bin\Debug\clientes.txt";
-        static string rutaProductos = @"C:\Users\ginko\Documents\GitHub\PruebaII_Trueque\PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel\PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel\bin\Debug\productos.txt";
+        static string rutaProducto = @"C:\Users\ginko\Documents\GitHub\PruebaII_Trueque\PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel\PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel\bin\Debug\productos.txt";
         #endregion
 
         #region Métodos
+
+        #region RellenarListaClientes
+        public static void RellenarListaCliente()
+        {
+            listaCliente = new List<Cliente>(); //nueva lista
+            using (StreamReader sr = new StreamReader(rutaCliente))
+            {
+                try 
+                { 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error -> "  + ex.ToString());
+                }
+                //Al estar usando 'using', se debe cerrar el stream (Close) y eliminar la info (Dispose).
+                sr.Dispose();
+                sr.Close();
+            }
+        }
+        #endregion
+
+        #region RellenarListaProductos
+        public static void RellenarListaProductos()
+        {
+            listaProducto = new List<Producto>(); //nueva lista
+            using (StreamReader sr = new StreamReader(rutaProducto))
+            {
+                try
+                {
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error -> " + ex.ToString());
+                }
+                //Al estar usando 'using', se debe cerrar el stream (Close) y eliminar la info (Dispose).
+                sr.Dispose();
+                sr.Close();
+            }
+        }
+        #endregion
+
         #endregion
     }
 }
