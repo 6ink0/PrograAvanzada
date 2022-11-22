708499917a4a9389f5ad487b0537dd4c074dfbcf
6ink0 <angel.6ink0@gmail.com>
2022-11-21 23:04:55 -0300

    Se crean List y rutas en la clase 'GestionProducto'. Se crean archivos txt en directorio local.

--- PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/GestionProducto.cs
+++ PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel/GestionProducto.cs
@@ -9,6 +9,10 @@ namespace PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel
     class GestionProducto
     {
         #region Propiedades
+        static List<Cliente> listaCliente;
+        static List<Producto> listaProducto;
+        static string rutaCliente = @"C:\Users\ginko\Documents\GitHub\PruebaII_Trueque\PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel\PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel\bin\Debug\clientes.txt";
+        static string rutaProductos = @"C:\Users\ginko\Documents\GitHub\PruebaII_Trueque\PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel\PruebaII_Trueque_Suarez_Fabian_Hernandez_Angel\bin\Debug\productos.txt";
         #endregion
 
         #region Métodos
